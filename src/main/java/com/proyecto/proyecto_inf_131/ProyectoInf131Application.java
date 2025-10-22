093cd7dc45c87bae02559cf11aad018c5ed76563
ionjpgDev <platacruzjhonatan@gmail.com>
2025-10-22 18:49:08 -0400

    conf_inicial

---
+++ src/main/java/com/proyecto/proyecto_inf_131/ProyectoInf131Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_inf_131;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInf131Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInf131Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/proyecto_inf_131/ProyectoInf131ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_inf_131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInf131ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
